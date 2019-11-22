22dbd750ba9d58ee6de826ae6d19ea2f62ee3a35
Chi-Mei Wang <kimmy0929@gmail.com>
2019-11-22 16:12:22 +0900

    revise spot model for geocode

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,6 @@
 class Spot < ApplicationRecord
-  geocoded_by :name
+  geocoded_by :address, if: ->(obj){obj.address.present?}
+  geocoded_by :name, if: ->(obj){obj.address.nil?}
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
   has_many :userspots, dependent: :destroy
 end
