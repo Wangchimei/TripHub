9ef2f678211bbbe7be4c7e00134c882e4268def7
Chi-Mei Wang <kimmy0929@gmail.com>
2019-11-27 19:28:37 +0900

    fix date range picker in edit view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
+  include TripsHelper
   before_action :authenticate_user!
 end
--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,9 @@
 module TripsHelper
+  def date_default
+    if action_name == "edit"
+    start_day = @trip.start_day.strftime("%Y-%m-%d").to_s
+    end_day = @trip.end_day.strftime("%Y-%m-%d").to_s
+    start_day + " - " + end_day
+    end
+  end
 end
