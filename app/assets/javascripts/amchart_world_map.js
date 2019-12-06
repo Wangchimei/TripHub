f709152c8b21ba857b2bf01c7e04e7d77fbd6599
Chi-Mei Wang <kimmy0929@gmail.com>
2019-12-06 15:53:07 +0900

    seperate js file in dashboard

---
+++ app/assets/javascripts/amchart_world_map.js
@@ -0,0 +1,40 @@
+var initialize_world_map;
+initialize_world_map = function() {
+  var visited_countries = gon.visited_countries;
+  function countriesOutput() {
+    var countriesData = [];
+    for (i = 0; i < visited_countries.length; i++) {
+      country = visited_countries[i];
+      countriesData.push({
+        id: country,
+        showAsSelected: true,
+      });
+    }
+    return countriesData;
+  }
+  var map = AmCharts.makeChart('mapdiv', {
+    type: 'map',
+    theme: 'dark',
+    projection: 'miller',
+    panEventsEnabled: false,
+    backgroundColor: '#00468C',
+    backgroundAlpha: 1,
+    zoomControl: {
+      zoomControlEnabled: true,
+    },
+    dataProvider: {
+      map: 'worldHigh',
+      getAreasFromMap: true,
+      areas: countriesOutput(),
+    },
+    areasSettings: {
+      autoZoom: false,
+      color: '#BFBFBF',
+      colorSolid: '#D3B288',
+      selectedColor: '#D3A66C',
+      outlineColor: '#2F3841',
+      rollOverColor: '#C0E0D1',
+      rollOverOutlineColor: '#2F3841',
+    },
+  });
+};
