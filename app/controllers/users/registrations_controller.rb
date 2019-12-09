a54de447f0280c7dca7c260f91639a06dd787a21
Chi-Mei Wang <kimmy0929@gmail.com>
2019-12-09 13:29:18 +0900

    fix devise pw update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -68,6 +68,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # Override the update_resource method
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    resource.update_without_current_password(params)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -53,4 +53,16 @@ class User < ApplicationRecord
   def unfollow!(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
