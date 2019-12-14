b5e4e8c7160f598d2be4e030a9325a57c75b5d39
Chi-Mei Wang <kimmy0929@gmail.com>
2019-12-15 01:16:57 +0900

    create rspec for spot model

--- spec/factories/to_countries.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :to_countries, class: 'ToCountry' do
-    association :trip
-    association :country
-  end
-end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a name' do
+    spot = Spot.new(
+      name: '',
+      address: '東京都港区芝公園４丁目２−８',
+      latitude: 35.6585805,
+      longitude: 139.7454329,
+      place_id: "a",
+    )
+    expect(spot).not_to be_valid
+  end
+
+  it 'is invalid without an address' do
+    spot = Spot.new(
+      name: '東京タワー',
+      address: '',
+      latitude: 35.6585805,
+      longitude: 139.7454329,
+      place_id: "a",
+    )
+    expect(spot).not_to be_valid
+  end
+
+  it 'is invalid for duplicate place ids' do
+    FactoryBot.create(:spot)
+    spot = Spot.new(
+      name: 'test',
+      address: 'test',
+      latitude: 35,
+      longitude: 139,
+      place_id: "ChIJCewJkL2LGGAR3Qmk0vCTGkg",
+    )
+    expect(spot).not_to be_valid
+  end
+
+  it 'is valid with required fields' do
+    FactoryBot.create(:spot)
+    spot = Spot.new(
+      name: '後楽園ホール',
+      address: '東京都文京区後楽１丁目３−６１',
+      latitude: 35.704259,
+      longitude: 139.7520446,
+      place_id: "ChIJrTs5XEeMGGARwSdc3mdD5T0",
+    )
+    expect(spot).to be_valid
+  end
 end
