a3c49ab47b7cd77faa1bcd6cb22cc5a70e66503a
Chi-Mei Wang <kimmy0929@gmail.com>
2019-12-16 21:07:48 +0900

    revise s3 variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,9 +7,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region:                'ap-northeast-1',
+      aws_access_key_id:     ENV["S3_ACCESS_KEY"],
+      aws_secret_access_key: ENV["S3_SECRET_KEY"],
+      region:                ENV["S3_REGION"],
       path_style:            true,
   }
 
@@ -24,9 +24,16 @@ CarrierWave.configure do |config|
   # config.cache_storage = :fog
 
   # 環境ごとにS3のバケットを指定
-  if Rails.env.production?
-    config.fog_directory = ENV['S3_BUCKET']
-    config.asset_host = 'https://triphub-production.s3-ap-northeast-1.amazonaws.com'
+  case Rails.env
+    when 'production'
+      config.fog_directory = ENV['S3_BUCKET']
+      config.asset_host = 'https://momento-production.s3-ap-northeast-1.amazonaws.com'
+    when 'development'
+      # config.fog_directory = 'momento-development'
+      # config.asset_host = 'https://momento-development.s3-ap-northeast-1.amazonaws.com'
+      # when 'test'
+      #   config.fog_directory = 'momento-test'
+      #   config.asset_host = 'https://momento-test.s3-ap-northeast-1.amazonaws.com'
   end
 end
 
