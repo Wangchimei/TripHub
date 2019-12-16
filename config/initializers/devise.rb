bed0030971a88f0038b673e746d51dd8984a26fe
Chi-Mei Wang <kimmy0929@gmail.com>
2019-12-16 20:45:55 +0900

    fix heroku deploy issue (key)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '965d94c1dffc64f3c424ab12ba98419b482a7e89b76063f3d8ed3ddbc68f6d1abce45179623786a0cee2a3aa3ab45044358e05f2c2a23a5123030a194838cc99'
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
