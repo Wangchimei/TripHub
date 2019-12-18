997e07fb55243f6e49183d3f885eeb5429faa38e
Chi-Mei Wang <kimmy0929@gmail.com>
2019-12-18 13:26:08 +0900

    revised unsplash api method

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,6 +1,6 @@
 Unsplash.configure do |config|
   config.application_access_key = ENV['SPLASH_ACCESS_KEY']
   config.application_secret = ENV['SPLASH_SECRET_KEY']
-  # config.application_redirect_uri = "https://your-application.com/oauth/callback"
+  # config.application_redirect_uri = "https://trip-hub.herokuapp.com/oauth/callback"
   config.utm_source = "triphub_app"
 end
