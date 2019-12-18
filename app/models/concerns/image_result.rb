dcdde6e3c9aa34b1f3c6776968b6742c6a63834f
Chi-Mei Wang <kimmy0929@gmail.com>
2019-12-18 13:24:38 +0900

    revised unsplash api method

--- app/models/concerns/image_result.rb
+++ app/models/concerns/image_result.rb
@@ -1,12 +1,16 @@
 module ImageResult
   extend ActiveSupport::Concern
   def self.get_url(search_term)
-    if Unsplash::Photo.search(search_term, 1, 1, :squarish)[0].nil?
+    connection = Unsplash::Photo.search(search_term).class
+    if connection != Unsplash::SearchResult
+      img_url = "https://imgur.com/PIz5jBj.png"
+    elsif Unsplash::Photo.search(search_term, 1, 1, :squarish)[0].nil?
       photo_id = Unsplash::Photo.search(search_term, 1, 1)[0].id
+      img_url = Unsplash::Photo.find(photo_id).add_utm_to_urls["regular"]
     else
       photo_id = Unsplash::Photo.search(search_term, 1, 1, :squarish)[0].id
+      img_url = Unsplash::Photo.find(photo_id).add_utm_to_urls["regular"]
     end
-    img_url = Unsplash::Photo.find(photo_id).add_utm_to_urls["regular"]
     # img_url.match(/^([^?]+)/)[0]
   end
 end
